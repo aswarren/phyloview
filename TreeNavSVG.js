1fab8b70723cf68db4ccbb2d0f9e2327461d9569
Andrew Warren <anwarren@vbi.vt.edu>
2016-11-18 00:26:16 -0500

    updating treenav to have height by id

--- TreeNavSVG.js
+++ TreeNavSVG.js
@@ -66,7 +66,7 @@ define([
     this.colorSpecies = customOptions.colorSpecies;
     this.colorGenus = customOptions.colorGenus;
     this.containerName=containerName;
-    this.idToHeight={};
+    this.idToHeight = {};
 
     // Calculate total nodes, max label length
 
@@ -111,11 +111,11 @@ define([
         this.tree = d3.layout.tree()
             .sort(null)
             .size([size.height, size.width - this.maxLabelLength*this.fontWidthForMargin])
-            .children(function(d)
+            .children(lang.hitch(this,function(d)
             {
                 this.idToHeight[d.id]=d.py;
                 return (!d.c || d.c.length === 0) ? null : d.c;
-            });
+            }));
 
         this.tipToColors  = this.getTipColors(this.colorGenus, this.colorSpecies);
 
